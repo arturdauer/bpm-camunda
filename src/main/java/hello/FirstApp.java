60a58d9b3802ccb364b2cd72e430e7d8343f3d43
arturdauer <arturdauer@gmail.com>
2020-10-14 15:28:21 +0200

    fix: camunda bpm eingerichtet

--- src/main/java/hello/FirstApp.java
+++ src/main/java/hello/FirstApp.java
@@ -4,8 +4,8 @@ import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@EnableProcessApplication("first-projekt")
+@SpringBootApplication(scanBasePackages = "helo")
+@EnableProcessApplication("app-bpm")
 public class FirstApp {
     public static void main(String[] args) {
         SpringApplication.run(FirstApp.class, args);
